d7492f8ab31187dd025a1faea8deec7d39de2b32
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2024-03-05 18:31:30 +0800

    fix: AgoraVideoView crash when dispose after RtcEngine.release (#1585)
    The `IrisRtcEngineRendering` handle becomes invalid after
    `RtcEngine.release`, which needs explicit clean-up of the texture
    renderers on the native side.
    
    Fix https://github.com/AgoraIO-Extensions/Agora-Flutter-SDK/issues/1567

--- windows/video_view_controller.cc
+++ windows/video_view_controller.cc
@@ -41,11 +41,7 @@ VideoViewController::VideoViewController(
 
 VideoViewController::~VideoViewController()
 {
-
-  for (auto itr = renderers_.begin(); itr != renderers_.end(); itr++)
-  {
-    itr->second->Dispose();
-  }
+  Dispose();
 }
 
 void VideoViewController::HandleMethodCall(
@@ -131,6 +127,11 @@ void VideoViewController::HandleMethodCall(
 
     result->Success(flutter::EncodableValue(true));
   }
+  else if (method.compare("dispose") == 0)
+  {
+    Dispose();
+    result->Success(flutter::EncodableValue(true));
+  }
   else if (method.compare("updateTextureRenderData") == 0)
   {
   }
@@ -177,4 +178,13 @@ bool VideoViewController::DestroyTextureRender(int64_t textureId)
     return true;
   }
   return false;
+}
+
+void VideoViewController::Dispose()
+{
+  for (const auto &entry : renderers_)
+  {
+    entry.second->Dispose();
+  }
+  renderers_.clear();
 }
